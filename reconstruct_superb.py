3fd9dd7b328f23f836b00baee7fe71a05c82f3d1
YuXiangLin <evan900527@gmail.com>
2024-07-17 13:49:55 +0800

    transfer data

--- reconstruct_superb.py
+++ reconstruct_superb.py
@@ -83,8 +83,9 @@ def main(args):
         os.makedirs("reconstructed", exist_ok=True)
         # source_name = source.split("/")[-1].split(".")[0]
         target_name = source.replace("ref_path", "syn_path")
-        if not os.path.exists(target_name.replace(os.get_basename(target_name), "")):
-            os.makedirs(target_name.replace(os.get_basename(target_name), ""))
+        if not os.path.exists(target_name.replace(os.path.basename(target_name), "")):
+            os.makedirs(target_name.replace(os.path.basename(target_name), ""))
+            
         torchaudio.save(target_name, full_pred_wave[0].cpu(), 24000)
 
 
