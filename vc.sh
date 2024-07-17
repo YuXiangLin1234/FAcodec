a02689df0209dc0dae1a412118512663e34e0bf9
YuXiangLin <evan900527@gmail.com>
2024-07-17 12:48:06 +0800

    transfer data

--- vc.sh
+++ vc.sh
@@ -1,3 +1,23 @@
 python reconstruct_redecoder.py \
+    --source "/workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3" \
+    --target "/workspace/chinese-audio-sample/old oti.wav" 
+
+python reconstruct_redecoder.py \
+    --source "/workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3" \
+    --target "/workspace/chinese-audio-sample/fu-xuan.wav" 
+
+python reconstruct_redecoder.py \
+    --source "/workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3" \
+    --target "/workspace/chinese-audio-sample/hook.wav" 
+
+python reconstruct_redecoder.py \
     --source "/workspace/chinese-audio-sample/old oti.wav" \
-    --target "/workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3"
+    --target "/workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3" 
+
+python reconstruct_redecoder.py \
+    --source "/workspace/chinese-audio-sample/hook.wav" \
+    --target "/workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3" 
+
+python reconstruct_redecoder.py \
+    --source "/workspace/chinese-audio-sample/fu-xuan.wav" \
+    --target "/workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3" 
