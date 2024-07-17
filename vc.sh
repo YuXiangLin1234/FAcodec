2f3ffb00efdc3bde6d7f12edc62d1475f03391a6
YuXiangLin <evan900527@gmail.com>
2024-07-17 12:17:19 +0800

    transfer data

--- vc.sh
+++ vc.sh
@@ -1,7 +1,7 @@
 python reconstruct_redecoder.py \
-    --source /workspace/chinese-audio-sample/old\ oti.wav
-    --target /workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3
-    --codec-ckpt-path /workspace/unofficial_facodec_ckpt/FAcodec/pytorch_model.bin
-    --redecoder-ckpt-path /workspace/unofficial_facodec_ckpt/FAcodec-redecoder/pytorch_model.bin
-    --codec-config-path /workspace/unofficial_facodec_ckpt/FAcodec/config.yml
-    --redecoder-config-path /workspace/unofficial_facodec_ckpt/FAcodec-redecoder/config.yml
+    --source /workspace/chinese-audio-sample/old\ oti.wav \
+    --target /workspace/chinese-audio-sample/ML Lecture 1_ Regression - Demo.mp3 \ 
+    --codec-ckpt-path /workspace/unofficial_facodec_ckpt/FAcodec/pytorch_model.bin \
+    --redecoder-ckpt-path /workspace/unofficial_facodec_ckpt/FAcodec-redecoder/pytorch_model.bin \
+    --codec-config-path /workspace/unofficial_facodec_ckpt/FAcodec/config.yml \
+    --redecoder-config-path /workspace/unofficial_facodec_ckpt/FAcodec-redecoder/config.yml 
